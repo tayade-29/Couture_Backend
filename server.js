00da8d3461780cd540804070e0b2385bfa5cc250
tayade-29 <tayadepranjal2903@gmail.com>
2025-09-26 18:48:59 +0530

    Backend ready to Deploy

--- server.js
+++ server.js
@@ -13,14 +13,42 @@ const PORT = 3000;
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/CoutureCollection')
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected to Atlas'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+//     const allowedOrigins = [
+//     "http://localhost:5173",
+//     "https://your-frontend-domain.com"
+// ];
+
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error("Not allowed by CORS"));
+//         }
+//     },
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+// }));
+
+
+// Middleware
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173", // frontend URL (adjust if needed)
+    origin: "*",   // allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for form data
 
